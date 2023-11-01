c1021c2fe2078eddf87135d43a199bed0e8dfd07
Shreya Dhame <31119988+shreyadhame@users.noreply.github.com>
2023-11-01 12:00:53 +0100

    Update calc_phi.py

--- calc_phi.py
+++ calc_phi.py
@@ -13,27 +13,20 @@ import sys
 import warnings
 warnings.filterwarnings('ignore')
 
+from adjustText import adjust_text
 import argparse
+import klepto
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.ma as ma
-import xarray as xr
 import pandas as pd
-from scipy import stats
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import pymannkendall as mk
 import proplot as pplt
-from adjustText import adjust_text
-import klepto
+import pymannkendall as mk
+from scipy import stats
 from scipy.stats import iqr
-
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
-
-pplt.rc['tick.lenratio']=0.02
-pplt.rc['savefig.transparent']=True
-
-plt.rcParams.update({'hatch.color': '#363636'})
+import xarray as xr
 
 #My modules 
 from load_le_data import * 
@@ -44,176 +37,82 @@ from func import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('type_sigma') #std | iqr
+    parser.add_argument('type_sigma') #std | iqr (standard deviation or interquartile range)
 
     args = parser.parse_args()
 
     type_sigma = str(args.type_sigma)
 
-    #Arrange models according to increasing EffCS 
-    mods = [gfdl, miroc6, miroc, giss, cesm1, nor, mpi, canesm2, access, gfdlcm3, csiro, ipsl, cnrm, cesm2, canesm5]
-    ecs = [2.44, 2.6 , 2.66, 2.71, 2.94, 3.03, 3.63, 3.7 , 3.88, 3.95, 4.09, 4.7 , 4.9 ,5.15, 5.64]
-    mod_units = ['K', 'C', 'C', 'C', 'C', 'C', 'K', 'K', 'C', 'K', 'K', 'C', 'C', 'K', 'C']
-
-    #convert to degC
-    mod_data = mods
-    for i in range(len(mods)):
-        if mod_units[i] == 'K':
-            mod_data[i] = mods[i] - 273.15
-        elif mod_units == 'C':
-            pass
-    
-    #Southern Ocean
-    lat1 = -45
-    lat2 = -65
-    lon1 = 0
-    lon2 = 360
-
-    so_obs_reg, lons, lats = sel_reg(tos_obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lon1,lon2=lon2)
-    so_mod_reg = [sel_reg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lon1,lon2=lon2)[0] for v in tos_moddata]
-    #Take spatial mean
-    so_obs_ts = wgt_mean(so_obs_reg,lons,lats)
-    so_mod_ts=[]
-    for i in range(len(so_mod_reg)):
-        so_mod_ts.append(np.stack([wgt_mean(ma.masked_where(v<-5,v),lons,lats) for v in so_mod_reg[i]]))
-    so_obs = so_obs_ts
-    so_mods = so_mod_ts
-
-    #Define time
-    years=np.unique(time.dt.year)
-
-    #Calculate observed trends for chunks of periods (E-W)
-    start_year = 1950
-    end_year = 2021
-
-    so_trends = []
-    for i in range(start_year,end_year):
-        for j in range(start_year,end_year):
-            ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
-            ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
-            chunk = so_obs[ind1:ind2] #Select a chunk
-            if len(chunk) >= 19: #*12: #Calculate trends only for >20 year chunks
-                so_trends.append(mk_test(chunk)[-1]*10) #len(chunk)) or 10 for decadal trend
-            else:
-                so_trends.append(np.nan) 
-
-    #East-west Pacific
-    #Select equatorial eastern Pacific region
+    #============================================================
+    ##East-west Pacific
+    #Eastern Pacific
     lat1 = 5
     lat2 = -5
     lone1 = -180%360
     lone2 = -80%360
+    #Western Pacific
+    lonw1 = 110
+    lonw2 = 180
+
+    #Select region
+    obs_reg1, lons, lats = selreg(obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lone1,lon2=lone2)
+    mod_reg1 = [selreg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lone1,lon2=lone2)[0] for v in mod_data]
     
-    obs_reg1, lons, lats = sel_reg(obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lone1,lon2=lone2)
-    mod_reg1 = [sel_reg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lone1,lon2=lone2)[0] for v in mod_data]
     #Take spatial mean
     obs_ts1 = wgt_mean(obs_reg1,lons,lats)
     mod_ts1=[]
     for i in range(len(mod_reg1)):
         mod_ts1.append([wgt_mean(ma.masked_where(v<-5,v),lons,lats) for v in mod_reg1[i]])
 
-    #Select equatorial western Pacific region
-    lonw1 = 110
-    lonw2 = 180
-
-    obs_reg2, lons, lats = sel_reg(obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lonw1,lon2=lonw2)
-    mod_reg2 = [sel_reg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lonw1,lon2=lonw2)[0] for v in mod_data]
+    #Select region
+    obs_reg2, lons, lats = selreg(obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lonw1,lon2=lonw2)
+    mod_reg2 = [selreg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lonw1,lon2=lonw2)[0] for v in mod_data]
+    mod_reg2[13] = ma.masked_where(mod_reg2[13] < 6., mod_reg2[13])
+    
     #Take spatial mean
     obs_ts2 = wgt_mean(obs_reg2,lons,lats) 
     mod_ts2=[]
     for i in range(len(mod_reg2)):
         mod_ts2.append([wgt_mean(ma.masked_where(v<0,v),lons,lats) for v in mod_reg2[i]])
 
-    # Calculate phi for chunks of periods
-    #Create empty array
-    ws_mphi = []
-    mti = []
-    #Select chunks
-    for m in range(len(ws_mods)):
-        ws_phi=[]
-        ti = []
-        start_year = 1950
-        end_year = 2021
-        for i in range(start_year,end_year):
-            for j in range(start_year,end_year):
-                ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
-                ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
-                chunk_obs = ws_obs[ind1:ind2]
-                chunk_mod = ws_mods[m][:,ind1:ind2]
-                if len(chunk_obs) >= 19: #Calculate trends only for >20 year chunks
-                    #Calculate phi
-                    ws_phi.append(calc_phi(chunk_mod,chunk_obs))
-                    ti.append(str(i)+'-'+str(j))
-                else:
-                    ws_phi.append(0.)
-                    ti.append('')
-        ws_mphi.append(np.array(ws_phi))
-        mti.append(np.array(ti))
-    ws_mphi = np.stack(ws_mphi)
-    mti = np.stack(mti)
-
-    #Calculate phi for chunks of periods
-    #Create empty array
-    so_mphi = []
-    #Select chunks
-    for m in range(len(so_mods)):
-        so_phi=[]
-        start_year = 1950
-        end_year = 2021
-        for i in range(start_year,end_year):
-            for j in range(start_year,end_year):
-                ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
-                ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
-                chunk_obs = so_obs[ind1:ind2]
-                chunk_mod = so_mods[m][:,ind1:ind2]
-                if len(chunk_obs) >= 19: #Calculate trends only for >20 year chunks
-                    #Calculate phi
-                    so_phi.append(calc_phi(chunk_mod,chunk_obs))
-                else:
-                    so_phi.append(0.)
-        so_mphi.append(np.array(so_phi))
-    so_mphi = np.stack(so_mphi)
-
-    #East-west gradient
+    #Calculate east-west gradient
     ew_obs = obs_ts1 - obs_ts2
     ew_mods = []
     for i in range(len(mod_ts2)):
         ew_mods.append(np.stack([np.array(mod_ts1[i][j] - mod_ts2[i][j]) for j in range(len(mod_ts2[i]))])) 
 
-    #Calculate observed trends for all time periods (E-W gradient)
-    start_year = 1950 
-    end_year = 2021
 
+    #Calculate observed trends for chunks of periods (E-W)
+    start_year = 1950
+    end_year = 2021
     ew_trends = []
     for i in range(start_year,end_year):
         for j in range(start_year,end_year):
             ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
             ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
-            #Select a chunk
-            chunk = ew_obs[ind1:ind2] 
-            #Calculate trends only for >19 year chunks
-            if len(chunk) >= 19: #*12: 
-                ew_trends.append(mk_test(chunk)[-1]*10) #decadal trend
+            chunk = ew_obs[ind1:ind2] #Select a chunk
+            if len(chunk) >= 19: #*12: #Calculate trends only for >20 year chunks
+                ew_trends.append(mk_test(chunk)[-1]*10) #len(chunk)) or 10 for decadal trend
             else:
                 ew_trends.append(np.nan)
 
-    #Calculate modeled trends for all time periods periods (E-W gradient)
-    #Ensemble mean of each model
-    modsm = [v.mean(axis=0) for v in reg_mods]
 
-    ew_mtrends = [[] for _ in range(len(modsm))]
-    for k in range(len(modsm)):
+    #Calculate modeled trends for chunks of periods (E-W)
+    #Model mean 
+    ew_modsm = [v.mean(axis=0) for v in ew_mods]
+    
+    ew_mtrends = [[] for _ in range(len(ew_modsm))]
+    for k in range(len(ew_modsm)):
         for i in range(start_year,end_year):
             for j in range(start_year,end_year):
                 ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
                 ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
-                #Select a chunk
-                chunk = modsm[k][ind1:ind2] 
-                #Calculate trends only for >19 year chunks
-                if len(chunk) >= 19: #*12: 
-                    ew_mtrends[k].append(mk_test(chunk)[-1]*10) #decadal trend
+                chunk = ew_modsm[k][ind1:ind2] #Select a chunk
+                if len(chunk) >= 19: #*12: #Calculate trends only for >20 year chunks
+                    ew_mtrends[k].append(mk_test(chunk)[-1]*10) #len(chunk)) or 10 for decadal trend
                 else:
                     ew_mtrends[k].append(np.nan)
+
 
     #Calculate phi for all time periods
     #Create empty arrays
@@ -256,12 +155,56 @@ if __name__ == "__main__":
     mub = np.stack(mub)
     mti = np.stack(mti)
 
-    #Calculate phi for chunks of periods (SO)
+    db = klepto.archives.dir_archive('mphi',serialized=True,cached=False)
+    db['ew_trends'] = ew_trends
+    db['ew_mtrends'] = ew_mtrends
+    db['ew_mphi'] = ew_mphi
+    db['mti'] = mti
+    db['mlb'] = mlb
+    db['mub'] = mub
+
+    #============================================================
+    #Southern Ocean SSTs
+    lat1 = -45
+    lat2 = -65
+    lon1 = 0
+    lon2 = 360
+
+    so_obs_reg, lons, lats = sel_reg(tos_obs,lon,lat,lat1=lat2,lat2=lat1,lon1=lon1,lon2=lon2)
+    so_mod_reg = [sel_reg(v,lon,lat,lat1=lat2,lat2=lat1,lon1=lon1,lon2=lon2)[0] for v in tos_moddata]
+    #Take spatial mean
+    so_obs_ts = wgt_mean(so_obs_reg,lons,lats)
+    so_mod_ts=[]
+    for i in range(len(so_mod_reg)):
+        so_mod_ts.append(np.stack([wgt_mean(ma.masked_where(v<-5,v),lons,lats) for v in so_mod_reg[i]]))
+    so_obs = so_obs_ts
+    so_mods = so_mod_ts
+
+    #Define time
+    years=np.unique(time.dt.year)
+
+    #Calculate observed trends for chunks of periods (E-W)
+    start_year = 1950
+    end_year = 2021
+
+    so_trends = []
+    for i in range(start_year,end_year):
+        for j in range(start_year,end_year):
+            ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
+            ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
+            chunk = so_obs[ind1:ind2] #Select a chunk
+            if len(chunk) >= 19: #*12: #Calculate trends only for >20 year chunks
+                so_trends.append(mk_test(chunk)[-1]*10) #len(chunk)) or 10 for decadal trend
+            else:
+                so_trends.append(np.nan) 
+
+    
+    #Calculate phi for chunks of periods
     #Create empty array
     so_mphi = []
     #Select chunks
     for m in range(len(so_mods)):
-        phi=[]
+        so_phi=[]
         start_year = 1950
         end_year = 2021
         for i in range(start_year,end_year):
@@ -272,24 +215,53 @@ if __name__ == "__main__":
                 chunk_mod = so_mods[m][:,ind1:ind2]
                 if len(chunk_obs) >= 19: #Calculate trends only for >20 year chunks
                     #Calculate phi
-                    phi.append(calc_phi(chunk_mod,chunk_obs))
-                elif np.isnan(chunk_mod).any() ==True:
-                    pass
+                    so_phi.append(calc_phi(chunk_mod,chunk_obs,type_sigma = type_sigma)[0])
                 else:
-                    phi.append(0.)
-        so_mphi.append(np.array(phi))
+                    so_phi.append(0.)
+        so_mphi.append(np.array(so_phi))
     so_mphi = np.stack(so_mphi)
 
-#Save to klepto
-db = klepto.archives.dir_archive('mphi',serialized=True,cached=False)
-db['ew_mphi'] = ew_mphi
-db['so_mphi'] = so_mphi
-db['mti'] = mti
-db['mlb'] = mlb
-db['mub'] = mub
-db['ew_obs'] = ew_obs
-db['ew_mods'] = ew_mods
-db['so_mods'] = so_mods
-db['ew_trends'] = ew_trends
-db['so_trends'] = so_trends
-db['ew_mtrends'] = ew_mtrends
+    #Save to klepto
+    db['so_trends'] = so_trends
+    db['so_mphi'] = so_mphi
+
+    #============================================================
+    #Central Pacific wind stress
+
+    
+    #Calculate phi for chunks of periods
+    #Create empty array
+    ws_mphi = []
+    mti = []
+    #Select chunks
+    for m in range(len(ws_mods)):
+        ws_phi=[]
+        ti = []
+        start_year = 1950
+        end_year = 2021
+        for i in range(start_year,end_year):
+            for j in range(start_year,end_year):
+                ind1 = np.where(time.dt.year==i)[0][0] #Index of start year
+                ind2 = np.where(time.dt.year==j)[0][0] #Index of end year
+                chunk_obs = ws_obs[ind1:ind2]
+                chunk_mod = ws_mods[m][:,ind1:ind2]
+                if len(chunk_obs) >= 19: #Calculate trends only for >20 year chunks
+                    #Calculate phi
+                    ws_phi.append(calc_phi(chunk_mod,chunk_obs))
+                    ti.append(str(i)+'-'+str(j))
+                else:
+                    ws_phi.append(0.)
+                    ti.append('')
+        ws_mphi.append(np.array(ws_phi))
+        mti.append(np.array(ti))
+    ws_mphi = np.stack(ws_mphi)
+    mti = np.stack(mti)
+
+
+
+
+
+
+
+
+
